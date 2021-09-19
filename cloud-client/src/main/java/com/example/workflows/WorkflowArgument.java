da4d49db061e810d219482386e29be8463750809
unknown <p.arunprasad0382@gmail.com>
2021-09-19 19:39:32 +0530

    Workflow rest service

---
+++ cloud-client/src/main/java/com/example/workflows/WorkflowArgument.java
@@ -0,0 +1,32 @@
+package com.example.workflows;
+
+public class WorkflowArgument {
+
+    private String projectname;
+    private String topicname;
+    private String data;
+
+    public String getProjectname() {
+        return projectname;
+    }
+
+    public void setProjectname(String projectname) {
+        this.projectname = projectname;
+    }
+
+    public String getTopicname() {
+        return topicname;
+    }
+
+    public void setTopicname(String topicname) {
+        this.topicname = topicname;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
